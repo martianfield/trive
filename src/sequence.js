062eccde0e4dd54525bfebfbb701c64d959a86b5
Martin Imfeld <martin.imfeld@gmail.com>
2016-04-09 07:06:36 +0200

    sequence creation

--- src/sequence.js
+++ src/sequence.js
@@ -1,9 +1,20 @@
 'use strict'
 
+// modules
 const merge = require('setthings').merge
 
+// exported 'enums'
+const modes = Object.freeze({
+  'Linear':0,
+  'Exponential':1
+})
+
+// exported methods
 const defaultOptions = {
-  cycles:1
+  initial:1,
+  cycles:1,
+  mode: modes.Linear,
+  increase: 1
 }
 
 const create = (options) => {
@@ -12,8 +23,29 @@ const create = (options) => {
     items: []
   }
 
+  let previousItem = undefined
   for(let i=0; i < options.cycles; i++) {
-    sequence.items.push({})
+    let item = {}
+
+    if(previousItem === undefined) {
+      item.value = options.initial
+    }
+    else {
+      switch(options.mode) {
+        case modes.Linear:
+          item.value = previousItem.value + options.increase
+          break
+        case modes.Exponential:
+          item.value = previousItem.value * options.increase
+          break
+        default:
+          item.value = options.initial
+          break
+      }
+    }
+
+    sequence.items.push(item)
+    previousItem = item
   }
 
   return sequence
@@ -21,3 +53,4 @@ const create = (options) => {
 
 
 module.exports.create = create
+module.exports.modes = modes
--- test/sequence.js
+++ test/sequence.js
@@ -2,9 +2,10 @@
 
 const expect = require('chai').expect
 const sequence = require(__dirname + '/../src/sequence')
+const setthings = require('setthings')
 
 describe("Sequence", () => {
-  it("number of items", () => {
+  it("Number of items", () => {
     // arrange
     let options = {
       cycles: 10
@@ -13,6 +14,38 @@ describe("Sequence", () => {
     let s = sequence.create(options)
     // assert
     expect(s.items.length).to.equal(10)
+  })
+
+  it("Linear Mode", () => {
+    // arrange
+    let options = {
+      initial:10,
+      cycles:3,
+      increase:5,
+      mode:sequence.modes.Linear
+    }
+    // act
+    let s = sequence.create(options)
+    // assert
+    expect(s.items[0].value).to.equal(10)
+    expect(s.items[1].value).to.equal(15)
+    expect(s.items[2].value).to.equal(20)
+  })
+
+  it("Exponential Mode", () => {
+    // arrange
+    let options = {
+      initial:10,
+      cycles:3,
+      increase:1.5,
+      mode:sequence.modes.Exponential
+    }
+    // act
+    let s = sequence.create(options)
+    // assert
+    expect(s.items[0].value).to.equal(10)
+    expect(s.items[1].value).to.equal(15)
+    expect(s.items[2].value).to.equal(22.5)
   })
 })
 
